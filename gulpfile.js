71fb25a2ed0b99e6fa3b194860ff89f24a8a3652
Sutangu <sutangu@gmail.com>
2018-02-09 11:49:05 +0600

    gulp and gitignore udpate

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
 var gulp = require('gulp');
+
+gulp.task('default', function() {
+    console.log('%cCheck it out yo', "background-color: #ffdd2d; color: #333; font-size: 16px;");
+});
+
+gulp.task('copy', function() {
+    gulp.src('index.html')
+        .pipe(gulp.dest('assets'))
+});
